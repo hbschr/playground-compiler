b335ddb0d70f2b8d95fbb717b2d6b77be50930fc
Heiko Hübscher <heiko.huebscher@gmail.com>
2018-11-26 18:54:48 +0100

    [haskell] add canonic formula printer and executer

--- Main.hs
+++ Main.hs
@@ -10,6 +10,18 @@ data Expression =
   | Sub Expression Expression
   deriving (Show)
 
+emit :: Expression -> String
+emit expr = case expr of
+  Num x -> [intToDigit x]
+  Add x y -> emit x ++ " + " ++ emit y
+  Sub x y -> emit x ++ " - " ++ emit y
+
+execute :: Expression -> Int
+execute expr = case expr of
+  Num x -> x
+  Add x y -> execute x + execute y
+  Sub x y -> execute x - execute y
+
 term :: Char -> Expression
 term x
   | isDigit x = Num (digitToInt x)
